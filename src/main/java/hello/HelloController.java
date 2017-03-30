b1e4ed2f57aff9fef9e00ab60554c8407de16145
sefire <sefire@mail.ru>
2017-03-30 12:30:15 +0300

    Initial commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,76 @@
+package hello;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import javax.annotation.PostConstruct;
+
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+
+    @PostConstruct
+    private void init() {
+        try {
+            String[] fieldsToMap = { "id", "about", "age_range", "birthday",
+                    "context", "cover", "currency", "devices", "education",
+                    "email", "favorite_athletes", "favorite_teams",
+                    "first_name", "gender", "hometown", "inspirational_people",
+                    "installed", "install_type", "is_verified", "languages",
+                    "last_name", "link", "locale", "location", "meeting_for",
+                    "middle_name", "name", "name_format", "political",
+                    "quotes", "payment_pricepoints", "relationship_status",
+                    "religion", "security_settings", "significant_other",
+                    "sports", "test_group", "timezone", "third_party_id",
+                    "updated_time", "verified", "viewer_can_send_gift",
+                    "website", "work" };
+
+            Field field = Class.forName(
+                    "org.springframework.social.facebook.api.UserOperations")
+                    .getDeclaredField("PROFILE_FIELDS");
+            field.setAccessible(true);
+
+            Field modifiers = field.getClass().getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, fieldsToMap);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        facebook.userOperations().getUserProfile().getEmail();
+        facebook.userOperations().getUserProfile().getId();
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
+    }
+
+}
